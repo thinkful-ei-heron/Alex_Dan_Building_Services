89a7e71d2025052cdc38af10dcb8b9180bd1d6f1
Alex Bannow <rbannal@gmail.com>
2019-11-06 12:12:54 -0500

    added functionality and testing for finding by id, updating item, deleting item, inserting item

--- src/shopping-list-service.js
+++ src/shopping-list-service.js
@@ -2,7 +2,31 @@
 
 const ShoppingService = {
   getAllItems(knex) {
-    return knex.select('*').from('shopping_list_test');
+    return knex.select('*').from('shopping_list');
+  },
+  insertItem(knex, item) {
+    return knex
+      .insert(item)
+      .into('shopping_list')
+      .returning('*')
+      .then(rows => rows[0]);
+  },
+  getById(knex, id) {
+    return knex
+      .select('*')
+      .from('shopping_list')
+      .where('id', id)
+      .first();
+  },
+  deleteItem(knex, id) {
+    return knex('shopping_list')
+      .delete()
+      .where({ id });
+  },
+  updateItem(knex, id, newData) {
+    return knex('shopping_list')
+      .update(newData)
+      .where({ id });
   }
 };
 
--- test/shopping-list-service.spec.js
+++ test/shopping-list-service.spec.js
@@ -1,3 +1,4 @@
+/* eslint-disable quotes */
 /* eslint-disable strict */
 const ShoppingService = require('../src/shopping-list-service.js');
 const knex = require('knex');
@@ -38,15 +39,15 @@ describe('Shopping list service object', function() {
     });
   });
 
-  before(() => db('shopping_list_test').truncate());
+  before(() => db('shopping_list').truncate());
 
-  afterEach(() => db('shopping_list_test').truncate());
+  afterEach(() => db('shopping_list').truncate());
 
   after(() => db.destroy());
 
   context(`Given 'shopping_list_test' has data`, () => {
     beforeEach(() => {
-      return db.into('shopping_list_test').insert(testItems);
+      return db.into('shopping_list').insert(testItems);
     });
     //retrieve all data
     it(`getAllItems() resolves all items from 'shopping_list_test' table`, () => {
@@ -54,12 +55,83 @@ describe('Shopping list service object', function() {
         expect(actual).to.eql(testItems);
       });
     });
-    //insert new item
-
+   
     //retrieve specific update
+    it(`getById() resolves an item by id from 'shopping_list' table`, () => {
+      const id = 2;
+      const testItem = testItems[id - 1];
+
+      return ShoppingService.getById(db, id).then(actual => {
+        expect(actual).to.eql({
+          id: id,
+          name: testItem.name,
+          price: testItem.price,
+          date_added: testItem.date_added,
+          checked: testItem.checked,
+          category: testItem.category
+        });
+      });
+    });
 
     //update item
-
+    it(`updateItem() updates an item from 'shopping_list' table`, () => {
+      const idOfItemToUpdate = 1;
+      const newItemData = {
+        name: 'updated name',
+        checked: true,
+        date_added: new Date()
+      };
+      return ShoppingService.updateItem(db, idOfItemToUpdate, newItemData)
+        .then(() => ShoppingService.getById(db, idOfItemToUpdate))
+        .then(item => {
+          expect(item).to.eql({
+            id: idOfItemToUpdate,
+            price: item.price,
+            category: item.category,
+            ...newItemData
+          });
+        });
+    });
     //delete item
+    it(`deleteItem() removes an item by id from 'shopping_list' table`, () => {
+      const id = 3;
+      return ShoppingService.deleteItem(db, id)
+        .then(() => ShoppingService.getAllItems(db))
+        .then(allItems => {
+          const expected = testItems.filter(item => item.id !== id);
+          expect(allItems).to.eql(expected);
+        });
+    });
+    
   });
+  context(`Given 'shopping_list' has no data`, () => {
+    it('getAllArticles() resolves an empty array', () => {
+      return ShoppingService.getAllItems(db).then(actual => {
+        expect(actual).to.eql([]);
+      });
+    });
+  
+    it(`insertItem() inserts a new item and resolves the new item`, () => {
+      const newItem = {
+        name: 'new product',
+        price: '15.00',
+        date_added: new Date('2020-01-01T00:00:00.000Z'),
+        checked: false,
+        category: 'Lunch'
+      };
+      return ShoppingService.insertItem(db, newItem).then(actual => {
+        expect(actual).to.eql({
+          id: 1,
+          name: newItem.name,
+          price: newItem.price,
+          date_added: newItem.date_added,
+          checked: newItem.checked,
+          category: newItem.category
+        });
+      });
+    });
+  });
+
 });
+
+
